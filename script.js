dbf4a1449c1fa0ee916ed01574805fb1f4a6b378
Module 3 <abuhayat5mmm@gmail.com>
2024-09-27 01:52:00 +0600

    js arow functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+
+
+
+let items = [32,34,54,23,45,33,31,12,24,13,22,14,44,54,37];
+
+let myItems = items.map(numbers => numbers*23);
+
+// console.log(myItems)
+
+// others systems 
+
+let studentsRoll = [234,345,322,456,342,312,345,654,342,456,343,342,567,545,342,564,433,453]
+
+let addRoll = roll => roll * 3 ;
+
+let calculations = studentsRoll.map(addRoll);
+
+console.log(...calculations)
+
+
+
